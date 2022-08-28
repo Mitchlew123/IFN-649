a14519a93b27a7ef946393f2d28cb4869466fdd1
Mitchlew123 <112307257+Mitchlew123@users.noreply.github.com>
2022-08-29 00:33:48 +1000

    Update humidPublish.py

--- Humidity_AWS/humidPublish.py
+++ Humidity_AWS/humidPublish.py
@@ -6,14 +6,13 @@ IP_ADDR = "204.236.175.9"
 print("Done")
 
 
-ser = serial.Serial("/dev/rfcomm0", 9600)
+ser = serial.Serial("/dev/rfcomm1", 9600)
 ser.write(str.encode('Start\r\n'))
 
 while True:
-	if ser.in_waiting > 0:
-		rawserial = ser.readline()
-		cookedserial = rawserial.decode('utf-8').strip('\r\n')
-		humidity = cookedserial[0:16]
-		print(cookedserial)
-		print (humidity)
-		publish.single("humid", humidity, hostname=IP_ADDR)
+        if ser.in_waiting > 0:
+                rawserial = ser.readline()
+                cookedserial = rawserial.decode('utf-8').strip('\r\n')
+                humidity = cookedserial[0:16]
+                print ("Sending humidity to AWS")
+                publish.single("humid", humidity, hostname=IP_ADDR)
